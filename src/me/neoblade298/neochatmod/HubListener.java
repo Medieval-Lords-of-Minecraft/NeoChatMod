c25d192b6407a3702a7ae9522a4db02f11104be2
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2023-01-24 10:45:23 -0800

    Update to hikaricp

--- src/me/neoblade298/neochatmod/HubListener.java
+++ src/me/neoblade298/neochatmod/HubListener.java
@@ -5,14 +5,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import me.neoblade298.neocore.util.Util;
+import me.neoblade298.neocore.bukkit.util.BukkitUtil;
+
 
 public class HubListener implements Listener {
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent e) {
 		Player p = e.getPlayer();
 		if (!p.hasPlayedBefore()) {
-			Util.msg(p, "You are muted until choosing a server! Use your compass or enter a portal!");
+			BukkitUtil.msg(p, "You are muted until choosing a server! Use your compass or enter a portal!");
 			e.setCancelled(true);
 			return;
 		}
